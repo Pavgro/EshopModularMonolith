3c966ebd90f8a13c5fd11e63d9cff2899aa22376
Pavel Grobov <pavelgrobov@gmail.com>
2025-05-19 21:13:50 +0300

    Remove Class1 and introduce DDD structure
    - Removed the `Class1` class from the project.
    - Updated `Shared.csproj` to include `MediatR` package.
    - Added `Aggregate<TId>` and `Entity<T>` abstract classes for DDD.
    - Defined `IAggregatge` and `IDomainEvent` interfaces for event handling.
    - Established `IEntity` interface for common entity properties.

---
+++ src/Shared/Shared/DDD/IAggregatge.cs
@@ -0,0 +1,13 @@
+﻿namespace Shared.DDD
+{
+    public interface IAggregatge<T> : IEntity<T>, IAggregatge
+    {
+        
+    }
+
+    public interface IAggregatge : IEntity
+    {
+        IReadOnlyList<IDomainEvent> DomainEvents { get; }
+        IDomainEvent[] ClearDomainEvents();
+    }
+}
